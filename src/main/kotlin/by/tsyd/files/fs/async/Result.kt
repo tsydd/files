4233175902e694e1d459a80e2ee00afb0e0c2534
Dmitry Tsydzik <dzmitry.tsydzik@softeq.com>
2018-08-14 12:41:40 +0300

    Extended result functionality to avoid instanceof checks

--- src/main/kotlin/by/tsyd/files/fs/async/Result.kt
+++ src/main/kotlin/by/tsyd/files/fs/async/Result.kt
@@ -6,8 +6,13 @@ import by.tsyd.files.fs.api.FsException
  * @author Dmitry Tsydzik
  * @since 6/19/18
  */
-@Suppress("unused")
-sealed class Result<T>
+sealed class Result<T> {
+    fun isOk(): Boolean = this is SuccessResult
+
+    fun asSuccess(): SuccessResult<T> = this as SuccessResult<T>
+
+    fun asError(): ErrorResult<T> = this as ErrorResult<T>
+}
 
 data class SuccessResult<T>(
     val data: T
