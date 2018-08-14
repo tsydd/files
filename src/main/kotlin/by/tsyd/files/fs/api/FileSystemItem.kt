28a1d86d8750daea47fc06c9676740d73a5e26a5
Dmitry Tsydzik <dzmitry.tsydzik@softeq.com>
2018-08-14 12:47:33 +0300

    Extended FileSystemItem functionality to avoid instanceof checks

--- src/main/kotlin/by/tsyd/files/fs/api/FileSystemItem.kt
+++ src/main/kotlin/by/tsyd/files/fs/api/FileSystemItem.kt
@@ -8,6 +8,14 @@ typealias FileContent = String?
 
 sealed class FileSystemItem {
     abstract val path: Path
+
+    fun isFile(): Boolean = this is File
+
+    fun isDirectory(): Boolean = this is Directory
+
+    fun asFile(): File = this as File
+
+    fun asDirectory(): Directory = this as Directory
 }
 
 data class File(
